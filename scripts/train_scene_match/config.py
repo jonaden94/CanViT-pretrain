a690cd751e9db1b220dd27cb698b99ff06e25690
Yohaï-Eliel Berreby <me@yberreby.com>
2025-12-21 20:03:51 -0500

    ~10 warmup epochs

--- scripts/train_scene_match/config.py
+++ scripts/train_scene_match/config.py
@@ -34,7 +34,7 @@ class Config:
     weight_decay: float = 0.05  # standard in ViTs
     n_viewpoints_per_step: int = 2  # Inner loop viewpoints
     min_viewpoint_scale: float = 0.125  # Minimum scale for random viewpoints
-    warmup_steps: int = 10_000
+    warmup_steps: int = 100_000
     grad_clip: float = 1.0
     n_steps: int = 2_000_000
     # Target normalization
