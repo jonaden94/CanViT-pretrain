6f5369303b2f015ac92866be2a019700a551c6c1
Yohaï-Eliel Berreby <me@yberreby.com>
2026-01-22 01:00:12 -0500

    env: activate uv venv

--- slurm/env.sh
+++ slurm/env.sh
@@ -57,4 +57,7 @@ else
     echo "[env] No comet_api_key.txt (optional)"
 fi
 
+# Activate venv just in case we forget to use `uv run`
+source "$UV_PROJECT_ENVIRONMENT/bin/activate"
+
 echo "[env] Done"
